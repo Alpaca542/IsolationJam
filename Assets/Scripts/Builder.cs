06d881200548e56d6661d23b96dba4fddad55867
Alpaca <mihaililyin08@gmail.com>
2024-04-27 11:06:20 +0400

    NeedTODoHomeworkWillFinishLater

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    public GameObject[] buildingPrefabs;
+    public bool IsBuildingStarted = false;
+    public GameObject CurrentBuilding;
+    public void OnBuildingStart(int BuildingNumber)
+    {
+        IsBuildingStarted = true;
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CurrentBuilding = Instantiate(buildingPrefabs[BuildingNumber], mouseWorldPos, Quaternion.identity);
+        CurrentBuilding.GetComponent<SpriteRenderer>().color = new Color32(0, 255, 0, 100);
+    }
+    private void Update()
+    {
+        if (IsBuildingStarted)
+        {
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CurrentBuilding.transform.position = mouseWorldPos;
+            if (CurrentBuilding.GetComponent<BuildingTriggerDetector>().AmITriggered)
+            {
+                CurrentBuilding.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 100);
+            }
+            else
+            {
+                CurrentBuilding.GetComponent<SpriteRenderer>().color = new Color32(0, 255, 0, 100);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                if (!CurrentBuilding.GetComponent<BuildingTriggerDetector>().AmITriggered)
+                {
+                    CurrentBuilding.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+                    CurrentBuilding.GetComponent<BoxCollider2D>().isTrigger = false;
+                    IsBuildingStarted = false;
+                    CurrentBuilding = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BuildingTriggerDetector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTriggerDetector : MonoBehaviour
+{
+    public bool AmITriggered;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AmITriggered = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        AmITriggered = false;
+    }
+}
