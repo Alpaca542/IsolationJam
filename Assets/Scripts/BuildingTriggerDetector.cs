09be37a9a91a950bc92418d26d17f1989701a90b
Alpaca <mihaililyin08@gmail.com>
2024-04-27 11:10:44 +0400

    fiixixiiixixixiix

--- Assets/Scripts/BuildingTriggerDetector.cs
+++ Assets/Scripts/BuildingTriggerDetector.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class BuildingTriggerDetector : MonoBehaviour
 {
     public bool AmITriggered;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         AmITriggered = true;
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         AmITriggered = false;
     }
