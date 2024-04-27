fd03237536748ad54f0c1850c40dc06d2e17c662
Areeoff <mailtoareeoff@gmail.com>
2024-04-27 18:07:30 +0500

    helpagainalpaacacacacaca

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     public float speed;
+    public bool AmIflying = false;
+    public float jump = 1;
+    public LayerMask groundlayer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,8 +18,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
         float dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX*speed,rb.velocity.y );
+        if(Physics2D.Raycast(transform.position,Vector2.down,30f,groundlayer))
+        {
+            AmIflying = false;
+            
+        }
+        else
+        {
+            AmIflying = true;
+        }
+        Debug.DrawRay(transform.position, Vector2.up,Color.white,10f);
         if (Mathf.Abs(dirX) > 0.2f)
         {
             if (dirX < 0 && transform.rotation != Quaternion.Euler(0, 180, 0) && transform.rotation != Quaternion.Euler(0, -180, 0))
@@ -32,6 +46,10 @@ public class Player : MonoBehaviour
         else
         {
             anim.SetBool("AmIWalking", false);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)&&!AmIflying) {
+            rb.AddForce(Vector2.up * jump);
+        
         }
     }
 }
