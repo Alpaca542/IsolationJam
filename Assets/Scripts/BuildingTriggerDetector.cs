8e95823e851e5e4e4435d8e01923bfd8404e9619
Alpaca <mihaililyin08@gmail.com>
2024-04-27 19:59:17 +0400

    FuckingBuilding

---
+++ Assets/Scripts/BuildingTriggerDetector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTriggerDetector : MonoBehaviour
+{
+    public bool AmITriggered;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        AmITriggered = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        AmITriggered = false;
+    }
+}
