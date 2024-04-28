cbbebfc7fd0b2de773b58c4f07ada02268c7dcb0
Alpaca <mihaililyin08@gmail.com>
2024-04-28 10:20:37 +0400

    InventoryStart

---
+++ Assets/Scripts/ManageInventory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageInventory : MonoBehaviour
+{
+    public RectTransform ButtonSword;
+    public RectTransform ButtonHammer;
+    public void OnHammerChosen()
+    {
+        ButtonSword.localScale = new Vector3(0.74f, 0.74f, 1f);
+        ButtonHammer.localScale = new Vector3(1f, 1f, 1f);
+    }
+    public void OnSwordChosen()
+    {
+        ButtonHammer.localScale = new Vector3(0.74f, 0.74f, 1f);
+        ButtonSword.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
